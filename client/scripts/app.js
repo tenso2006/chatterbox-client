fa37ec6edc2174ccfad54fa44943b8e7670876f9
tenso2006 <tenso2006@gmail.com>
2017-01-02 17:35:13 -0800

    Able to send and fetch message

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,7 +6,10 @@ var App = function() {
 };
 
 App.prototype.init = function() {
-
+  var context = this;
+  $('.username').on('click', {context: context}, context.handleUsernameClick);
+  $('#send .submit').on('click', {context: context}, context.handleSubmit);
+  context.fetch(context);
 };
 
 App.prototype.send = function(message) {
@@ -25,18 +28,30 @@ App.prototype.send = function(message) {
   });
 };
 
-App.prototype.fetch = function() {
+App.prototype.fetch = function(context, previousData = null) {
   $.ajax({
+    url: 'https://api.parse.com/1/classes/messages',
     type: 'GET',
-    contentType: 'application/json',
+    contentType: 'jsonp',
+    data: {
+      order: '-createdAt'
+    },
     success: function (data) {
-      console.log('chatterbox: Message sent', data);
+      var currentData = JSON.stringify(data.results[0]);
+      if ( previousData !== currentData ) {
+        context.renderMessage(JSON.parse(currentData));
+      }
+      previousData = currentData;
+      setTimeout(function() {
+        context.fetch(context, previousData);
+      }, 1000);
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to send message', data);
     }
   });
+  
 };
 
 App.prototype.clearMessages = function() {
@@ -45,11 +60,26 @@ App.prototype.clearMessages = function() {
 };
 
 App.prototype.renderMessage = function(message) {
-  $('#chats').append(`<div>${message.username}: ${message.text} @ ${message.roomname}</div>`);
+  $('#chats').append(`<div> <a class='username' href='#'>${message.username}</a>: ${message.text} @ ${message.roomname}</div>`);
 };
 
 App.prototype.renderRoom = function(roomName) {
   $('#roomSelect').append(`<div>${roomName}</div>`);
 };
 
-var app = new App;  
+App.prototype.handleUsernameClick = function(event) {
+
+};
+
+App.prototype.handleSubmit = function(event) {
+  var text = $('#message').val();
+  var message = {
+    username: 'DJ',
+    text: text,
+    roomname: 'lobby'
+  };
+  event.data.context.send(message);
+  $('#message').val('');
+};
+
+var app = new App();
